1c1f22c65ee9e9ebfa33a38c00c0f722cea8ff1e
msilis <mikssilis@gmail.com>
2026-05-04 22:19:33 +0100

    fix env

--- functions/api/upload.ts
+++ functions/api/upload.ts
@@ -1,7 +1,7 @@
 import type { R2Bucket, EventContext } from "@cloudflare/workers-types";
 
 interface Env {
-  BUCKET: R2Bucket;
+  CLOUDFLARE_BUCKET_NAME: R2Bucket;
   FAMILY_PIN: string;
 }
 
@@ -28,7 +28,7 @@ export const onRequestPost = async (
   const key = `animations/${Date.now()}-${file.name}`;
 
   // @ts-ignore - DOM ReadableStream is compatible at runtime
-  await env.BUCKET.put(key, file.stream(), {
+  await env.CLOUDFLARE_BUCKET_NAME.put(key, file.stream(), {
     httpMetadata: {
       contentType: file.type,
     },
