934e3ddd2ac3dc47223f2c345401e8769d6e97fb
msilis <mikssilis@gmail.com>
2026-05-14 17:17:28 +0100

    working towards local upload

--- functions/api/upload.ts
+++ functions/api/upload.ts
@@ -9,6 +9,7 @@ interface Env {
   CLOUDFLARE_ACCESS_KEY_ID: string;
   CLOUDFLARE_SECRET_KEY: string;
   CLOUDFLARE_ID: string;
+  ENVIRONMENT: string;
 }
 
 interface UploadRequest {
@@ -27,19 +28,22 @@ export const onRequestPost = async (
   const { title, who, fileName, contentType, pin } =
     (await request.json()) as UploadRequest;
 
-  console.log({ title, who, contentType, fileName });
-
   if (pin !== env.FAMILY_PIN) {
     return new Response("Invalid pin", { status: 401 });
   }
 
+  const isLocal = env.ENVIRONMENT === "development";
+
   const s3 = new S3Client({
     region: "auto",
-    endpoint: `https://${env.CLOUDFLARE_ID}.r2.cloudflarestorage.com`,
+    endpoint: isLocal
+      ? "http://localhost:8788/rest/v1/s3"
+      : `https://${env.CLOUDFLARE_ID}.r2.cloudflarestorage.com`,
     credentials: {
-      accessKeyId: env.CLOUDFLARE_ACCESS_KEY_ID,
-      secretAccessKey: env.CLOUDFLARE_SECRET_KEY,
+      accessKeyId: isLocal ? "S3RVER" : env.CLOUDFLARE_ACCESS_KEY_ID,
+      secretAccessKey: isLocal ? "S3RVER" : env.CLOUDFLARE_SECRET_KEY,
     },
+    forcePathStyle: isLocal,
   });
 
   const key = `animations/${Date.now()}-${fileName}`;
